88627f1e7caf6f0ab76c5a016551d7bac56db440
Jimmy Lu <jimmy.lu@sunbirddcim.com>
2025-07-07 07:18:27 -0500

    grid cell

--- projects/gnro/grid/src/components/grid-cell/grid-cell.component.ts
+++ projects/gnro/grid/src/components/grid-cell/grid-cell.component.ts
@@ -1,4 +1,5 @@
-import { ChangeDetectionStrategy, Component, input } from '@angular/core';
+import { ChangeDetectionStrategy, Component, input, computed } from '@angular/core';
+import { GnroColumnConfig, GnroColumnWidth, GnroGridConfig, GnroGridSetting } from '../../models/grid.model';
 
 @Component({
   selector: 'gnro-grid-cell',
@@ -10,10 +11,16 @@ import { ChangeDetectionStrategy, Component, input } from '@angular/core';
     '[class.row-even-sticky]': 'sticky() && rowIndex() % 2 === 1',
     '[class.row-odd-sticky]': 'sticky() && rowIndex() % 2 === 0',
     '[class.selected]': 'sticky() && selected()',
+
+    '[style.height]': 'height$()',
   },
 })
 export class GnroGridCellComponent {
+  gridConfig = input.required<GnroGridConfig>();
   sticky = input.required<boolean>();
   selected = input.required<boolean>();
   rowIndex = input.required<number>();
+
+  height$ = computed(() => `${this.gridConfig().rowHeight}px`);
 }
+//  [style.height]="gridConfig().rowHeight + 'px'"
