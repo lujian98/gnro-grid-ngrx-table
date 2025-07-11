c71365625f5b8e0c5c54a14f78c535a354eccf01
Jimmy Lu <lujian98@yahoo.com>
2025-07-11 07:25:14 -0500

    remove old radio

--- projects/gnro/fields/src/radio-group-field/radio-group-field.component.ts
+++ projects/gnro/fields/src/radio-group-field/radio-group-field.component.ts
@@ -28,7 +28,6 @@ import {
   GnroLabelDirective,
   GnroLabelWidthDirective,
 } from '@gnro/ui/form-field';
-// import { GnroRadioComponent } from '@gnro/ui/radio';
 import { GnroRadioComponent, GnroRadioGroupDirective } from '@gnro/ui/radio-group';
 import { TranslatePipe } from '@ngx-translate/core';
 import { GnroFieldsErrorsComponent } from '../field-errors/field-errors.component';
