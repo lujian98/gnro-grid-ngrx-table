56597f74b4295232495466d8ad138e3d38d23f85
Jimmy Lu <lujian98@yahoo.com>
2025-09-14 12:14:59 -0500

    extends store

--- projects/gnro-demo/src/app/views/window/demos/data/app-store/+state/app-store.reducer.ts
+++ projects/gnro-demo/src/app/views/window/demos/data/app-store/+state/app-store.reducer.ts
@@ -32,9 +32,47 @@ function combineReducers(
   };
 }
 
+console.log(' baseStoreReducer=', baseStoreReducer);
+console.log(' appStoreReducer=', appStoreReducer);
 const extendedReducer = combineReducers([baseStoreReducer, appStoreReducer]);
+
+export const concatReducers =
+  <T, V extends Action = Action>(...reducers: Array<ActionReducer<T>>) =>
+  (state: T, action: Action): T =>
+    reducers.reduce((accumulatedState, reducer) => reducer(accumulatedState, action), state);
+
+export const concatReducerFactories =
+  <T, V extends Action = Action>(...reducerFactories: Array<(state: T, action: Action) => T>) =>
+  (state: T, action: Action): T =>
+    reducerFactories.reduce((accumulatedState, reducerFactory) => reducerFactory(accumulatedState, action), state);
+
+//const newReducers = concatReducers([baseStoreReducer, appStoreReducer]);
+
+//const newReducers = concatReducerFactories([baseStoreReducer, appStoreReducer]);
+
+console.log(' extendedReducer=', extendedReducer);
+
+function mergeReducers<TState>(...reducers: ActionReducer<TState, Action>[]): ActionReducer<TState, Action> {
+  const mergedReducers = (state: TState, action: Action): TState => {
+    if (reducers.length === 0) {
+      throw new Error('At least one reducer was expected');
+    }
+    const newState = reducers.reduce((initialState, reducer) => {
+      const intermediaryState = reducer(initialState, action);
+      return intermediaryState;
+    }, state);
+
+    return newState as TState;
+  };
+  return mergedReducers as ActionReducer<TState, Action>;
+}
+
+const concatedReducers = mergeReducers(
+  baseStoreReducer as ActionReducer<unknown, Action<string>>,
+  appStoreReducer as ActionReducer<unknown, Action<string>>,
+);
 
 export const appStoreFeature = createFeature({
   name: 'baseStore',
-  reducer: extendedReducer,
+  reducer: concatedReducers as ActionReducer<AppStoreState, Action<string>>,
 });
