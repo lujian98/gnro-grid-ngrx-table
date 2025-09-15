2912c449899ad53fda9fd73d6b4390e75d5b44b2
Jimmy Lu <lujian98@yahoo.com>
2025-09-15 18:44:58 -0500

    share reducers

--- projects/gnro-demo/src/app/views/window/demos/data/app-store/+state/app-store.selectors.ts
+++ projects/gnro-demo/src/app/views/window/demos/data/app-store/+state/app-store.selectors.ts
@@ -2,7 +2,7 @@ import { appStoreFeature } from './app-store.reducer';
 
 //can only have selectBaseStoreState since must use baseStore,
 //also cannot access selectTotal or any new attributes not belong to base store.
-export const { selectBaseStoreState, selectTotal } = appStoreFeature;
+export const { selectBaseStoreState, selectTotal, selectData } = appStoreFeature;
 
 /*
 //use this to access select attributes not in the base store if use mergeReducers
