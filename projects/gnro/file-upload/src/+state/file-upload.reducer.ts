887658b88c997aa9a3849a31ee61ededfb5bace9
Jimmy Lu <lujian98@yahoo.com>
2025-09-17 20:28:35 -0500

    grid GnroOnAction

--- projects/gnro/file-upload/src/+state/file-upload.reducer.ts
+++ projects/gnro/file-upload/src/+state/file-upload.reducer.ts
@@ -1,4 +1,4 @@
-import { GnroUploadFile } from '@gnro/ui/core';
+import { GnroOnAction, GnroUploadFile } from '@gnro/ui/core';
 import { createFeature, createReducer, on } from '@ngrx/store';
 import { fileUploadActions } from './file-upload.actions';
 
@@ -22,33 +22,36 @@ export function getFileUpload(fieldName: string, file: File, relativePath: strin
   };
 }
 
+export const gnroFileUploadOnActions: GnroOnAction<FileUploadState>[] = [
+  on(fileUploadActions.dropFile, (state, { relativePath, file }) => {
+    const fieldName = `file_drop_upload_${state.uploadFiles.length + 1}`;
+    return {
+      ...state,
+      uploadFiles: [...state.uploadFiles, getFileUpload(fieldName, file, relativePath)],
+    };
+  }),
+  on(fileUploadActions.selectFile, (state, { fieldName, file }) => {
+    const uploadFiles = [...state.uploadFiles].filter((item) => item.fieldName !== fieldName);
+    return {
+      ...state,
+      uploadFiles: [...uploadFiles, getFileUpload(fieldName, file, '')],
+    };
+  }),
+  on(fileUploadActions.clearSelectedFile, (state, { fieldName }) => {
+    return {
+      ...state,
+      uploadFiles: [...state.uploadFiles].filter((item) => item.fieldName !== fieldName),
+    };
+  }),
+  on(fileUploadActions.uploadSuccess, fileUploadActions.clearFiles, (state) => ({
+    ...state,
+    uploadFiles: [],
+  })),
+];
+
+export const gnroFileUploadReducer = createReducer(initialState, ...gnroFileUploadOnActions);
+
 export const gnroFileUploadFeature = createFeature({
   name: 'gnroFileUpload',
-  reducer: createReducer(
-    initialState,
-    on(fileUploadActions.dropFile, (state, { relativePath, file }) => {
-      const fieldName = `file_drop_upload_${state.uploadFiles.length + 1}`;
-      return {
-        ...state,
-        uploadFiles: [...state.uploadFiles, getFileUpload(fieldName, file, relativePath)],
-      };
-    }),
-    on(fileUploadActions.selectFile, (state, { fieldName, file }) => {
-      const uploadFiles = [...state.uploadFiles].filter((item) => item.fieldName !== fieldName);
-      return {
-        ...state,
-        uploadFiles: [...uploadFiles, getFileUpload(fieldName, file, '')],
-      };
-    }),
-    on(fileUploadActions.clearSelectedFile, (state, { fieldName }) => {
-      return {
-        ...state,
-        uploadFiles: [...state.uploadFiles].filter((item) => item.fieldName !== fieldName),
-      };
-    }),
-    on(fileUploadActions.uploadSuccess, fileUploadActions.clearFiles, (state) => ({
-      ...state,
-      uploadFiles: [],
-    })),
-  ),
+  reducer: gnroFileUploadReducer,
 });
