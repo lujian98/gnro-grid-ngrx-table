b5fd44254dc3f5931f7011f1ab2907cd4478c39e
Jimmy Lu <lujian98@yahoo.com>
2025-09-20 16:26:24 -0500

    rename data to extends store

--- projects/gnro-demo/src/app/views/state/state.routes.ts
+++ projects/gnro-demo/src/app/views/state/state.routes.ts
@@ -1,5 +1,5 @@
 import { Routes } from '@angular/router';
-import { AppDataStoreComponent } from './demos/data/data-store.component';
+import { AppDataStoreComponent } from './demos/extends/data-store.component';
 import { AppStateComponent } from './state.component';
 
 export const AppStateRoutes: Routes = [
