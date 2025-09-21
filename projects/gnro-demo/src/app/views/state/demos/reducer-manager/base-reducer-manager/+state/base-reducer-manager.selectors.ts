21d8039abe8bb04bb9e78dd742e3f82b6230fac2
Jimmy Lu <jimmy.lu@sunbirddcim.com>
2025-09-21 18:49:03 -0500

    select feature

--- projects/gnro-demo/src/app/views/state/demos/reducer-manager/base-reducer-manager/+state/base-reducer-manager.selectors.ts
+++ projects/gnro-demo/src/app/views/state/demos/reducer-manager/base-reducer-manager/+state/base-reducer-manager.selectors.ts
@@ -1,18 +1,15 @@
 import { createSelector } from '@ngrx/store';
+import { createFeatureSelector } from '@ngrx/store';
 import { BaseReducerManagerState } from './base-reducer-manager.reducer';
 
 export interface FeatureReducerManagerState {
   [key: string]: BaseReducerManagerState;
 }
 
+export const featureSelector = (featureName: string) => (state: FeatureReducerManagerState) => state[featureName];
+
 export const selectConfig = (featureName: string) =>
-  createSelector(
-    (state: FeatureReducerManagerState) => state[featureName],
-    (state: BaseReducerManagerState) => state.config,
-  );
+  createSelector(featureSelector(featureName), (state: BaseReducerManagerState) => state.config);
 
 export const selectData = (featureName: string) =>
-  createSelector(
-    (state: FeatureReducerManagerState) => state[featureName],
-    (state: BaseReducerManagerState) => state.data,
-  );
+  createSelector(featureSelector(featureName), (state: BaseReducerManagerState) => state.data);
