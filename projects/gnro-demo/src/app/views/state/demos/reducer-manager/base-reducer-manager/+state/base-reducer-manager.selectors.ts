87d1a6ac38b888c70dbc8149657c1d3757263a67
Jimmy Lu <lujian98@yahoo.com>
2025-09-20 22:28:13 -0500

    add reducer manager

--- projects/gnro-demo/src/app/views/state/demos/reducer-manager/base-reducer-manager/+state/base-reducer-manager.selectors.ts
+++ projects/gnro-demo/src/app/views/state/demos/reducer-manager/base-reducer-manager/+state/base-reducer-manager.selectors.ts
@@ -1,12 +1,12 @@
 import { createSelector } from '@ngrx/store';
 import { BaseReducerManagerState } from './base-reducer-manager.reducer';
 
-export interface FeatureState {
+export interface FeatureReducerManagerState {
   [key: string]: BaseReducerManagerState;
 }
 
 export const selectData = (featureName: string) =>
   createSelector(
-    (state: FeatureState) => state[featureName],
+    (state: FeatureReducerManagerState) => state[featureName],
     (state: BaseReducerManagerState) => state.data,
   );
