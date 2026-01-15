537b4edd637b45f6e791bc9e0fec1ae7753f45e9
Jimmy Lu <jimmy.lu@sunbirddcim.com>
2026-01-15 09:44:11 -0600

    base

--- projects/gnro-demo/src/app/views/entity-tabs/src/locations/location-tabs.component.ts
+++ projects/gnro-demo/src/app/views/entity-tabs/src/locations/location-tabs.component.ts
@@ -1,16 +1,17 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, ViewChild } from '@angular/core';
 import { GnroTabConfig, GnroTabsComponent, GnroTabsConfig } from '@gnro/ui/tabs';
 import { AppStockChartComponent } from '../../../d3/demos/stock-charts/stock-chart.component';
 import { AppGridMultiRowSelectionComponent } from '../../../grid/remote-data/grid-multi-row-selection.component';
 import { AppGridRemoteVirtualScrollComponent } from '../../../grid/remote-data/grid-virtual-scroll.component';
 import { AppLocationEntityComponent } from './panels/location-entity.component';
+import { GnroButtonComponent } from '@gnro/ui/button';
 
 @Component({
   selector: 'app-location-tabs',
   templateUrl: './location-tabs.component.html',
   styleUrls: ['./location-tabs.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
-  imports: [GnroTabsComponent, AppLocationEntityComponent],
+  imports: [GnroTabsComponent, AppLocationEntityComponent, GnroButtonComponent],
 })
 export class AppLocationTabsComponent {
   tabsConfig: Partial<GnroTabsConfig> = {
@@ -24,16 +25,22 @@ export class AppLocationTabsComponent {
       closeable: false,
     },
     {
-      name: 'two',
-      title: 'Two3',
-      content: AppStockChartComponent,
-      closeable: false,
-      //disabled: true,
-    },
-    {
-      name: 'three',
+      name: 'Entity1',
       content: AppLocationEntityComponent,
       closeable: true,
     },
   ];
+
+  @ViewChild(GnroTabsComponent, { static: false }) tabsPanel!: GnroTabsComponent<unknown>;
+  private totalTabs = this.tabs.length;
+  newTab(): void {
+    const tabName = `Tab${this.totalTabs + 1}`;
+    const item = {
+      name: tabName,
+      content: AppLocationEntityComponent,
+      closeable: true,
+    };
+    this.tabsPanel.addTab(item);
+    this.totalTabs++;
+  }
 }
