502e61d946f3df5ebf10f4bfeeb8e5a2c36d4848
Jimmy Lu <jimmy.lu@sunbirddcim.com>
2026-01-15 20:52:33 -0600

    form pages

--- projects/gnro-demo/src/app/views/entity-tabs/src/locations/panels/location-entity.component.ts
+++ projects/gnro-demo/src/app/views/entity-tabs/src/locations/panels/location-entity.component.ts
@@ -1,5 +1,6 @@
-import { ChangeDetectionStrategy, Component, inject, OnInit, effect } from '@angular/core';
+import { ChangeDetectionStrategy, Component, inject, OnInit, effect, OnDestroy } from '@angular/core';
 import { FormControl, FormGroup, FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { toSignal } from '@angular/core/rxjs-interop';
 import { GnroTextFieldComponent, GnroTextFieldConfig, defaultTextFieldConfig } from '@gnro/ui/fields';
 import { GnroButtonComponent } from '@gnro/ui/button';
 import { GnroLayoutComponent, GnroLayoutHeaderComponent } from '@gnro/ui/layout';
@@ -19,12 +20,14 @@ import { EntityTabsFacade } from '../../libs/entity-tabs/+state/entity-tabs.faca
     GnroTextFieldComponent,
   ],
 })
-export class AppLocationEntityComponent implements OnInit {
+export class AppLocationEntityComponent implements OnInit, OnDestroy {
   private entityTabsFacade = inject(EntityTabsFacade);
+
   tabId: string = '';
   form: FormGroup = new FormGroup({
     nodeCode: new FormControl(''),
   });
+  private readonly formValues = toSignal(this.form.valueChanges);
 
   fieldConfig: GnroTextFieldConfig = {
     ...defaultTextFieldConfig,
@@ -37,13 +40,43 @@ export class AppLocationEntityComponent implements OnInit {
 
   constructor() {
     effect(() => {
-      const tab = this.entityTabsFacade.getActiveTab();
-      console.log('pppppptab', tab());
+      // const tab = this.entityTabsFacade.getActiveTab();
+
+      // console.log('tabId', this.tabId);
+      const tab = this.entityTabsFacade.getTabById(this.tabId);
+      console.log('ttt', tab());
       this.form.patchValue(tab()!.values);
+    });
+
+    effect(() => {
+      const values = this.formValues();
+      const tab = this.entityTabsFacade.getActiveTab()()!;
+      console.log('values', values);
+      /*
+      this.entityTabsFacade.updateTab({
+        ...tab,
+        values,
+      });
+      */
     });
   }
 
   ngOnInit(): void {
     console.log('tabId', this.tabId);
+  }
+
+  ngOnDestroy(): void {
+    const tab = this.entityTabsFacade.getActiveTab()()!;
+    const values = this.formValues();
+    console.log('88888 ngOnDestroy =', values);
+    const newtab = {
+      ...tab,
+      values: {
+        ...tab.values,
+        ...values,
+      },
+    };
+    console.log('88888 newtab =', newtab);
+    this.entityTabsFacade.updateTab(newtab);
   }
 }
